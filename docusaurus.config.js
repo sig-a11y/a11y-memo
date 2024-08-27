a0d6d1a95c293df92cfac96b57a665901a830876
inky <git@wo-class.cn>
2024-08-27 15:59:12 +0800

    site: 显示最后编辑时间

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,10 +43,9 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/sig-a11y/a11y-memo/tree/main/',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
